fe00e31aeb8e1c09d0359edb40360223e5ae0982
Eneas Junior <eneas.junior@avenuecode.com>
2024-11-06 10:49:18 -0300

    feat - atualizacao da main

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import time
 from llm_api.genia import generate
 from streamlit_auth.streamlit_auth import streamlit_authenticator
 from utils import *
+import traceback
 
 config = get_environments()
 # escrevendo um título na página
@@ -14,7 +15,7 @@ with cols[0]:
     st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyIQYNRPrt6QIvrg6PCK1t82JTL7G6uVwk7w&s")
 with cols[1]:
     st.title("Chatbot interativo")
-    st.caption("(Ver : Em desenvolvimento")
+    st.caption("(Ver : Em desenvolvimento)")
 import os
 
 
@@ -25,6 +26,7 @@ except stauth.LoginError as e:
     st.error(e)
 
 if st.session_state['authentication_status']:
+    stop = False
     authenticator.logout()
     st.write(f'Bem vindo *{st.session_state["name"]}*')
     st.title('Poc Chatbot!')
@@ -33,6 +35,22 @@ if st.session_state['authentication_status']:
         st.session_state.initialized = True
     if 'kept_username' not in st.session_state:
         st.session_state['kept_username'] = st.session_state['username']
+
+    uploaded_file = st.file_uploader("Faça upload de uma base para extração de indicadores:", type="xlsx")
+    if stop:
+        st.stop()
+    if uploaded_file is not None:
+        with st.spinner("Processando a base..."):
+            try:
+                arquivo_conteudo = uploaded_file.getvalue()
+                arquivo_nome = uploaded_file.name                    
+                df = excel_to_dataframe(config.get('folder_xlsx'))
+                st.success("Nova base carregado com sucesso!", icon="✅")
+            except Exception as e:
+                error_trace = traceback.format_exc()
+                st.html(f"<font color='red'><b>O processo foi interrompido devido ao seguinte erro:</b></font>")
+                st.error(f"Erro ao anexar a base: {error_trace}")
+                stop = True                
 
     # Store LLM generated responses
     if "messages" not in st.session_state.keys():
@@ -48,12 +66,17 @@ if st.session_state['authentication_status']:
         st.session_state.messages.append({"role": "user", "content": prompt})
         st.chat_message("user").markdown(prompt)
 
+    if stop:
+        st.stop()
     # Generate a new response if last message is not from assistant
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
             with st.spinner("Pensando..."):
-                df = excel_to_dataframe(config.get('folder_xlsx'))
-                responses = generate(pergunta=prompt, df=df)
-                st.markdown(responses.text)
-        message = {"role": "assistant", "content": responses.text}
-        st.session_state.messages.append(message)
+                if df is not None:
+                    responses = generate(pergunta=prompt, df=df)
+                    st.markdown(responses.text)
+                    message = {"role": "assistant", "content": responses.text}
+                    st.session_state.messages.append(message)                    
+                else:
+                    st.error('para que eu possa responder a pergunta, anexe um arquivo')
+                    stop = True
